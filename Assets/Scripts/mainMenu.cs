b56f3b88dda6b0060db1d67699b9c20229defda7
infernodragon456 <vighumandy420@gmail.com>
2025-04-17 13:17:21 +0530

    integrate ui

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -7,11 +7,12 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {   
         //add name of the scene you have given
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("GameScene1");
     }
 
     public void QuitGame()
     {
+        Debug.Log("Quit button clicked - Application would quit in build");
         Application.Quit();
     }
 }
